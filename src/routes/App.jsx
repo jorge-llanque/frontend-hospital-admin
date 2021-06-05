7d13ca0fb967587a1f472662fa0a7f0c961de4f8
jorge <georgellanque@gmail.com>
2021-06-05 17:30:32 -0400

    Add Login

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Login from '../components/Login';
 
-const App = () => (
-        <div>Hello world</div>
-    )
-
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Login} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
 export default App;
