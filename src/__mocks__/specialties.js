c69fd12e9d6d8e2f127e354e7b945dc065a5329c
jorge <georgellanque@gmail.com>
2021-06-06 19:38:50 -0400

    Add specialty toolbar

---
+++ src/__mocks__/specialties.js
@@ -0,0 +1,152 @@
+export default [
+    {
+      "id": "1",
+      "name": "nam",
+      "description": "Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.\n\nIn quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.",
+      "avatar": "http://dummyimage.com/169x100.png/cc0000/ffffff"
+    }, {
+      "id": "2",
+      "name": "vitae",
+      "description": "In congue. Etiam justo. Etiam pretium iaculis justo.\n\nIn hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.",
+      "avatar": "http://dummyimage.com/90x88.png/ff4444/ffffff"
+    }, {
+      "id": "3",
+      "name": "integer",
+      "description": "Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.\n\nPraesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio.\n\nCras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
+      "avatar": "http://dummyimage.com/78x91.png/cc0000/ffffff"
+    }, {
+      "id": "4",
+      "name": "pede",
+      "description": "Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.\n\nCras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.",
+      "avatar": "http://dummyimage.com/186x51.png/dddddd/000000"
+    }, {
+      "id": "5",
+      "name": "sapien",
+      "description": "Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.",
+      "avatar": "http://dummyimage.com/190x67.png/dddddd/000000"
+    }, {
+      "id": "6",
+      "name": "nulla",
+      "description": "Fusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem.",
+      "avatar": "http://dummyimage.com/94x91.png/ff4444/ffffff"
+    }, {
+      "id": "7",
+      "name": "fusce",
+      "description": "Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.",
+      "avatar": "http://dummyimage.com/91x85.png/ff4444/ffffff"
+    }, {
+      "id": "8",
+      "name": "rhoncus",
+      "description": "In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.\n\nNulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.",
+      "avatar": "http://dummyimage.com/228x76.png/5fa2dd/ffffff"
+    }, {
+      "id": "9",
+      "name": "convallis",
+      "description": "Curabitur at ipsum ac tellus semper interdum. Mauris ullamcorper purus sit amet nulla. Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.",
+      "avatar": "http://dummyimage.com/163x50.png/dddddd/000000"
+    }, {
+      "id": "10",
+      "name": "aliquet",
+      "description": "Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum.\n\nIn hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.\n\nAliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.",
+      "avatar": "http://dummyimage.com/80x99.png/cc0000/ffffff"
+    }, {
+      "id": "11",
+      "name": "augue",
+      "description": "Sed ante. Vivamus tortor. Duis mattis egestas metus.\n\nAenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.",
+      "avatar": "http://dummyimage.com/73x68.png/5fa2dd/ffffff"
+    }, {
+      "id": "12",
+      "name": "lacinia",
+      "description": "Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.",
+      "avatar": "http://dummyimage.com/201x84.png/ff4444/ffffff"
+    }, {
+      "id": "13",
+      "name": "sed",
+      "description": "Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.\n\nIn sagittis dui vel nisl. Duis ac nibh. Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus.\n\nSuspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst.",
+      "avatar": "http://dummyimage.com/120x82.png/5fa2dd/ffffff"
+    }, {
+      "id": "14",
+      "name": "nisi",
+      "description": "Vestibulum quam sapien, varius ut, blandit non, interdum in, ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis.\n\nDuis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus.\n\nMauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.",
+      "avatar": "http://dummyimage.com/69x68.png/cc0000/ffffff"
+    }, {
+      "id": "15",
+      "name": "cum",
+      "description": "Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.\n\nSed ante. Vivamus tortor. Duis mattis egestas metus.",
+      "avatar": "http://dummyimage.com/219x64.png/5fa2dd/ffffff"
+    }, {
+      "id": "16",
+      "name": "lacus",
+      "description": "In congue. Etiam justo. Etiam pretium iaculis justo.\n\nIn hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.\n\nNulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.",
+      "avatar": "http://dummyimage.com/178x92.png/5fa2dd/ffffff"
+    }, {
+      "id": "17",
+      "name": "bibendum",
+      "description": "Phasellus in felis. Donec semper sapien a libero. Nam dui.",
+      "avatar": "http://dummyimage.com/184x86.png/cc0000/ffffff"
+    }, {
+      "id": "18",
+      "name": "etiam",
+      "description": "In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.\n\nAliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.",
+      "avatar": "http://dummyimage.com/55x64.png/cc0000/ffffff"
+    }, {
+      "id": "19",
+      "name": "leo",
+      "description": "Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.\n\nCurabitur at ipsum ac tellus semper interdum. Mauris ullamcorper purus sit amet nulla. Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.",
+      "avatar": "http://dummyimage.com/120x84.png/5fa2dd/ffffff"
+    }, {
+      "id": "20",
+      "name": "vestibulum",
+      "description": "In quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.\n\nMaecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui.",
+      "avatar": "http://dummyimage.com/74x68.png/5fa2dd/ffffff"
+    }, {
+      "id": "21",
+      "name": "feugiat",
+      "description": "Aenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum.\n\nCurabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.",
+      "avatar": "http://dummyimage.com/111x88.png/5fa2dd/ffffff"
+    }, {
+      "id": "22",
+      "name": "in",
+      "description": "Fusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem.\n\nSed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus.",
+      "avatar": "http://dummyimage.com/181x88.png/5fa2dd/ffffff"
+    }, {
+      "id": "23",
+      "name": "fusce",
+      "description": "Proin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis. Ut at dolor quis odio consequat varius.\n\nInteger ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi.\n\nNam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.",
+      "avatar": "http://dummyimage.com/95x93.png/dddddd/000000"
+    }, {
+      "id": "24",
+      "name": "dictumst",
+      "description": "Morbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.\n\nFusce consequat. Nulla nisl. Nunc nisl.\n\nDuis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum.",
+      "avatar": "http://dummyimage.com/207x87.png/cc0000/ffffff"
+    }, {
+      "id": "25",
+      "name": "nulla",
+      "description": "Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.\n\nQuisque id justo sit amet sapien dignissim vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est. Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros.",
+      "avatar": "http://dummyimage.com/136x72.png/5fa2dd/ffffff"
+    }, {
+      "id": "26",
+      "name": "sapien",
+      "description": "Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.",
+      "avatar": "http://dummyimage.com/244x63.png/ff4444/ffffff"
+    }, {
+      "id": "27",
+      "name": "pulvinar",
+      "description": "Proin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.",
+      "avatar": "http://dummyimage.com/92x72.png/cc0000/ffffff"
+    }, {
+      "id": "28",
+      "name": "libero",
+      "description": "Vestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.\n\nIn congue. Etiam justo. Etiam pretium iaculis justo.",
+      "avatar": "http://dummyimage.com/91x58.png/5fa2dd/ffffff"
+    }, {
+      "id": "29",
+      "name": "sed",
+      "description": "Quisque id justo sit amet sapien dignissim vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est. Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros.\n\nVestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.",
+      "avatar": "http://dummyimage.com/154x71.png/dddddd/000000"
+    }, {
+      "id": "30",
+      "name": "consequat",
+      "description": "Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.\n\nProin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.",
+      "avatar": "http://dummyimage.com/89x81.png/dddddd/000000"
+    }];
---
+++ src/components/specialty/SpecialtyCard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function SpecialtyCard() {
+    return (
+        <div>
+            Specialty Card
+        </div>
+    )
+}
