efce5c92cd334fda7a0e4f8b32b78c49b066f8ff
jorge <georgellanque@gmail.com>
2021-06-07 08:59:29 -0400

    Add settings notifications

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import { Box, Container} from '@material-ui/core';
+import SettingsNotifications from '../components/settings/SettingsNotifications';
+import SettingsPassword from '../components/settings/SettingsPassword';
 
-export default function Settings() {
-    return (
-        <div>
-            Settings
-        </div>
-    )
-}
+const SettingsView = () => (
+    <>
+      <Box
+        sx={{
+            backgroundColor: 'background.default',
+            minHeight: '100%',
+            py: 3
+        }}
+      >
+          <Container maxWidth="lg" >
+            <SettingsNotifications />
+            <Box sx={{ pt: 3 }} >
+                <SettingsPassword />
+            </Box>
+          </Container>
+      </Box>
+    </>
+);
+
+export default SettingsView;
